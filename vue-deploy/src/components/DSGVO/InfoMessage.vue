252509be5f6d3884266074e85f7044e3a7c8b952
Cedric Arnhold <Cedric.Arnhold@Student.HTW-Berlin.de>
2024-11-11 09:51:56 +0100

    update InfoMessage.vue

--- vue-deploy/src/components/DSGVO/InfoMessage.vue
+++ vue-deploy/src/components/DSGVO/InfoMessage.vue
@@ -1,13 +1,26 @@
-### Erstellt von Cedric visit my [GitHub](https://cedric-cj.github.io/AboutMe/)
 <template>
   <div class="info-message">
     <div>
       <p>
-        Diese Seite nutzt keine Cookies, diesbezüglich wird laut
-          <a href="https://dr-dsgvo.de/tmg-paragraph-15-nutzungsdaten/" target="_blank">§ 15 DDG</a>
-          kein Cookie Manager für diese Webseite benötigt.
+        Diese Seite nutzt keine Cookies, daher ist kein Cookie-Manager erforderlich.
+      </p>
+      <p>
+        <span class="show-more-link" @click="showMore = !showMore">
+          {{ showMore ? 'Weniger anzeigen' : 'Mehr anzeigen' }}
+        </span>
+      </p>
+      <div v-if="showMore" class="extended-info">
+        <p>
+          Gemäß <a href="https://www.gesetze-im-internet.de/ttdsg/__25.html" target="_blank">§ 25 Abs. 2 Nr. 2 TTDSG</a> wird auf dieser Website keine Speicherung oder Verarbeitung von Informationen im Endgerät des Nutzers durch Cookies oder ähnliche Technologien vorgenommen. Da die Nutzung von Cookies zur Bereitstellung dieser Webseite nicht erforderlich ist, entfällt die Notwendigkeit eines Cookie-Managers oder einer Einwilligung des Nutzers in die Datenverarbeitung nach <a href="https://gdpr-info.eu/art-12-gdpr/" target="_blank">Art. 12 DSGVO</a>.
         </p>
+        <p>
+          Art. 12 DSGVO regelt die Verpflichtung zur transparenten Information der Nutzer über die Verarbeitung personenbezogener Daten und die damit verbundenen Rechte. Da auf dieser Website jedoch keine entsprechenden Daten durch Cookies oder ähnliche Technologien verarbeitet werden, entfällt die Pflicht zur Einholung einer Einwilligung.
+        </p>
+      </div>
       <button @click="closeMessage">Verstanden</button>
+    </div>
+    <div class="footer-credits">
+      <b href="#" @click.prevent="confirmRedirect">Erstellt von Cedric</b>
     </div>
   </div>
 </template>
@@ -15,6 +28,11 @@
 <script>
 export default {
   name: 'InfoMessage',
+  data() {
+    return {
+      showMore: false
+    };
+  },
   methods: {
     confirmRedirect() {
       if (confirm('Sie verlassen jetzt die Seite und gelangen zur Seite des Erstellers dieser Seite. Möchten Sie fortfahren?')) {
@@ -43,14 +61,12 @@ export default {
   border-radius: 15px;
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
 }
-
 .info-message p {
+  color: white;
   margin: 0;
   padding: 0;
 }
-
-.footer-credits
-{
+.footer-credits {
   color: gray;
   font-size: 10px;
   padding: 2px;
@@ -58,12 +74,16 @@ export default {
   cursor: pointer;
   display: inline-block;
 }
-
+.show-more-link {
+  color: #f39c12;
+  text-decoration: underline;
+  cursor: pointer;
+  font-size: 0.9em;
+}
 .info-message a {
   color: #f39c12;
   text-decoration: none;
 }
-
 .info-message button {
   margin-top: 10px;
   padding: 10px;
@@ -73,8 +93,12 @@ export default {
   cursor: pointer;
   border-radius: 5px;
 }
-
 .info-message button:hover {
   background-color: #e08e0b;
+}
+.extended-info {
+  margin-top: 10px;
+  text-align: justify;
+  font-size: 0.8em;
 }
 </style>
